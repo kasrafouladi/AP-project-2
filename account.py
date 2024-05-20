03400aa8766f1401291ace27cbc4008dff40a40e
Hamta Rahmani <hamtar693@gmail.com>
2024-05-20 13:58:55 +0330

    تغییزات جزیی

--- account.py
+++ account.py
@@ -1,9 +1,9 @@
 import basic as b
+import project as p   
 
 class Account:
     def __init__(self, username, isnew):
         self.name = username
-        self.hname = b.hash(username)
         if isnew == True:
             self.create()
         else:
@@ -11,13 +11,13 @@ class Account:
         self.show_dashboard()
 
     def create(self):
-        b.os.mkdir('./accounts/' + self.hname)
-        b.os.mkdir('./projects/' + self.hname)
-        f = open('projects/projects_list.json', 'a')
+        b.os.mkdir('./accounts/' + self.name)
+        b.os.mkdir('./projects/' + self.name)
+        f = open('projects/' + self.name + '/projects_list.json', 'a')
         f.close()
-        f = open('accounts/' + self.hname + '/invitations.txt', 'a')
+        f = open('accounts/' + self.name + '/invitations.txt', 'a')
         f.close()
-        f = open('accounts/' + self.hname + '/message.txt', 'a')
+        f = open('accounts/' + self.name + '/message.txt', 'a')
         f.close()
 
     def backup(self):
@@ -42,7 +42,12 @@ class Account:
         pass
 
     def send_message(self):
-        
+        b.head()
+        print('From: ' + self.name)
+        print('To: ')
+        print('Subject: ')
+        print('_' * 50)
+        b.start_from()
         pass
 
     def show_messages(self):
@@ -58,11 +63,18 @@ class Account:
         pass
 
     def show_projects(self):
-        
+        # project -> name -> project list
+        # entekhab
+        # p = p.project(folan)
+        # p.show()
         pass
 
     def create_projects(self):
-        cnt = 0
-        if cnt != 0:
-            self.projectlist.update("untiteled" + str(cnt) : ['projects/' + self.hname + '/' + str(b.time.time()), 4])
+        self.counter += 1            #name : [path, acess_level]
+        self.projectlist.update({"board" + str(self.counter) : ['projects/' + self.name + '/' + str(b.time.time()), 4]})
+        b.os.mkdir('projects/' + self.name + '/' + str(b.time.time()))
+        p.Project('projects/' + self.name + '/' + str(b.time.time()))
         pass
+    
+    
+Account('123', True)
---
+++ project.py
@@ -0,0 +1,40 @@
+import basic as b
+'''
+class Project:
+    def __init__(self, name, p_addres, new):
+        if new == True:
+            self.create(name, p_addres)
+        else:
+            self.backup(name, p_addres)
+        pass
+    
+    def create(self, name, p_addres):
+        
+        pass
+'''    
+    
+def upload(s):
+    file = open('table.txt', 'w')
+    file.write(s)
+
+def download():
+    file = open('table.txt', 'r')
+    return file.read()
+
+def main():
+    while True:
+        b.head()
+        upload("")
+        cor = input().split()
+        b.start_from(cor[0], cor[1])
+        print('press a key to upload it into  system')
+        b.getch()
+        b.head()
+        print('press a key to dowload it rom system')
+        b.getch()
+        b.head()
+        s = download()
+        print(s)
+        
+    
+main()
